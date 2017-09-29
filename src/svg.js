0db0990fa4d5805656eca85bccc247a319e010d6
Rune Madsen <rune@runemadsen.com>
2017-09-29 10:46:27 +0800

    rect and circle parsing working

--- src/svg.js
+++ src/svg.js
@@ -1,29 +1,26 @@
 var Rune = require("rune.js");
-var isomorphicLoad = require('./load');
+var isomorphicLoad = require("./load");
 
 // The xmldoc package makes it hard to stub the external because
 // it is required as .DOMParser, so we do this instead to make it
 // work in both node and browser (without the lib).
 var IsomorphicDOMParser;
-var xmldom = require('xmldom');
-if(xmldom.DOMParser) {
+var xmldom = require("xmldom");
+if (xmldom.DOMParser) {
   IsomorphicDOMParser = xmldom.DOMParser;
-}
-else {
+} else {
   IsomorphicDOMParser = DOMParser;
 }
 
 var Svg = function(url) {
-  console.log(DOMParser);
-  if(url.match(/\<svg/)) {
+  if (url.match(/\<svg/)) {
     this.svg = url;
   } else {
     this.url = url;
   }
-}
+};
 
 Svg.prototype = {
-
   load: function(cb) {
     var that = this;
     isomorphicLoad(this.url, function(err, data) {
@@ -32,13 +29,135 @@ Svg.prototype = {
     });
   },
 
-  toGroup: function() {
+  toGroup: function(x, y) {
+    if (!this.svg) {
+      throw Error("You must use load() before generating font paths");
+    }
 
-    if(!this.svg) throw Error("You must use load() before generating font paths");
-
-    // DO A BUNCH OF THINGS
-
+    var group = new Rune.Group(x || 0, y || 0);
+    var parser = new IsomorphicDOMParser();
+    var el = parser.parseFromString(this.svg);
+    var svgel = findChildByTag(el, "svg");
+    var shapes = domChildrenToGroupChildren(group, svgel.childNodes);
+    return group;
   }
+};
+
+// Helpers
+// ---------------------------------------------------
+
+function domChildrenToGroupChildren(group, childNodes) {
+  for (var i = 0; i < childNodes.length; i++) {
+    var child = childNodes[i];
+
+    if (child.tagName == "rect") {
+      var s = fillShape(new Rune.Rectangle(), child, {
+        x: "x",
+        y: "y",
+        width: "width",
+        height: "height",
+        rx: "rx",
+        ry: "ry",
+        transform: ["rotation"]
+      });
+      group.add(s);
+    } else if (child.tagName == "ellipse") {
+      var s = fillShape(new Rune.Ellipse(), child, {
+        cx: "x",
+        cy: "y",
+        rx: "rx",
+        ry: "ry",
+        transform: ["rotation"]
+      });
+      group.add(s);
+    }
+    // if ellipse
+    // if circle
+    // line
+    // path
+    // polygon
+    // group
+    // rectangle
+    // text
+  }
+}
+
+// This function loops through the node attributes based on
+// the hash map keys, gets the attributes, and adds them to
+// the shape state based on the hash values.
+function fillShape(shape, node, map) {
+  var keys = Object.keys(map);
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var v = map[k];
+
+    // if this is transform, handle it based on allowed vars in array
+    if (k == "transform") {
+      var transformVal = node.getAttribute("transform");
+
+      if (v.indexOf("rotation") > -1) {
+        var rotateMatch = transformVal.match(/rotate\((.+)\)/);
+        if (rotateMatch) {
+          var nums = rotateMatch[1].split(" ");
+          shape.state.rotation = parseInt(nums[0]);
+          if (nums[1]) {
+            shape.state.rotationX = parseInt(nums[1]);
+          }
+          if (nums[2]) {
+            shape.state.rotationY = parseInt(nums[2]);
+          }
+        }
+      }
+    } else if (typeof v == "string") {
+      // if this is just a string, assign to that state var
+      var attributeVal = node.getAttribute(k);
+      if (attributeVal) {
+        shape.state[v] = parseInt(attributeVal);
+      }
+    }
+  }
+  shape.changed();
+  return shape;
+}
+
+function parseTransform(transformString) {
+  var attrs = {};
+
+  if (rotateMatch) {
+    var nums = rotateMatch[1].split(" ");
+    attrs.rotation = parseInt(nums[0]);
+    if (nums[1]) {
+      attrs.rotationX = parseInt(nums[1]);
+    }
+    if (nums[2]) {
+      attrs.rotationY = parseInt(nums[2]);
+    }
+  }
+  return attrs;
+}
+
+function getAtttributes(node, keys) {
+  var attrs = {};
+  for (var i = 0; i < keys.length; i++) {
+    var val = node.getAttribute(keys[i]);
+    if (val) {
+      if (keys[i] == "transform") {
+        Object.assign(attrs, parseTransform(val));
+      } else {
+        attrs[keys[i]] = val;
+      }
+    }
+  }
+  return attrs;
+}
+
+function findChildByTag(node, tagName) {
+  for (var i = 0; i < node.childNodes.length; i++) {
+    if (node.childNodes[i].tagName == tagName) {
+      return node.childNodes[i];
+    }
+  }
+  return null;
 }
 
 module.exports = Svg;
--- test/both/specs.js
+++ test/both/specs.js
@@ -1,9 +1,8 @@
 describe("Rune.SVG", function() {
-
-  var svgFile = typeof window === 'undefined' ? "test/tiger.svg" : "tiger.svg";
+  var testFile = "test/test.svg";
+  var tigerFile = "test/tiger.svg";
 
   describe("constructor", function() {
-
     it("should set svg", function() {
       var svg = new Rune.Svg("<svg></svg>");
       expect(svg.url).toBeUndefined();
@@ -11,22 +10,56 @@ describe("Rune.SVG", function() {
     });
 
     it("should set url", function() {
-      var svg = new Rune.Svg(svgFile);
-      expect(svg.url).toEqual(svgFile);
+      var svg = new Rune.Svg(testFile);
+      expect(svg.url).toEqual(testFile);
       expect(svg.svg).toBeUndefined();
     });
-
   });
 
   describe("load()", function() {
     it("should load the SVG", function(done) {
-      var svg = new Rune.Svg(svgFile);
+      var svg = new Rune.Svg(testFile);
       svg.load(function(err) {
         expect(err).toBeNull();
         expect(svg.svg).not.toBeUndefined();
-        done()
+        done();
       });
     });
   });
 
+  describe("toGroup()", function() {
+    fit("should parse the SVG and return a Rune.Group", function(done) {
+      var svg = new Rune.Svg(testFile);
+      svg.load(function(err) {
+        var group = svg.toGroup();
+
+        // Rect
+        expect(group.children[0].type).toEqual("rectangle");
+        expect(group.children[0]).toHaveState({
+          x: 1,
+          y: 2,
+          width: 3,
+          height: 4,
+          rx: 5,
+          ry: 6,
+          rotation: 9,
+          rotationX: 10,
+          rotationY: 11
+        });
+
+        // Ellipse
+        expect(group.children[1].type).toEqual("ellipse");
+        expect(group.children[1]).toHaveState({
+          x: 1,
+          y: 2,
+          rx: 3,
+          ry: 4,
+          rotation: 7,
+          rotationX: 8,
+          rotationY: 9
+        });
+        done();
+      });
+    });
+  });
 });
